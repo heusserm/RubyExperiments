9a06d27b1ea5f01f422453122e09fe95f9e1db15
heusserm <matt.heusser@gmail.com>
2017-10-31 11:04:48 -0400

    using a template

--- wiki/wiki.rb
+++ wiki/wiki.rb
@@ -3,9 +3,24 @@ require "sinatra"
 #Accept requests from outside my computer
 set :bind, "0.0.0.0"
 
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+  return new
+end
+
 get "/" do
   erb :welcome
 end
+
+get "/test" do
+  erb :test
+end
+
+get "/:title" do
+  page_content(params[:title])
+end
+
 
 
 
