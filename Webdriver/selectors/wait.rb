2fab28fa3b8bdbd96ca205100f8947892d0a6115
heusserm <matt.heusser@gmail.com>
2018-10-15 14:16:28 -0400

    Created a new wait scheme example

---
+++ Webdriver/selectors/wait.rb
@@ -0,0 +1,33 @@
+require "selenium-webdriver"
+require 'test/unit'
+
+
+class SampleTest < Test::Unit::TestCase 
+
+    def setup
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('--ignore-certificate-errors')
+      options.add_argument('--disable-popup-blocking')
+      options.add_argument('--disable-translate')
+      @driver = Selenium::WebDriver.for :chrome, options: options
+   end
+
+   def teardown
+      @driver.quit
+   end
+
+   def test_css_focus
+      
+      #Only tested on mac - finds sample.html in the current working directory
+      directory = File.expand_path File.dirname(__FILE__)
+      specific_filename = "file://" + directory + "/sample.html"
+      @driver.navigate.to specific_filename
+
+
+     element = @driver.find_element :xpath, "//h2[text() = 'Sample Text']"
+     assert_equal(element.attribute('innerText'),"Sample Text")
+
+     sleep(3)
+   end
+
+end
