b2cbb2ede262de94c08d2f62e13bfe3d31e32efd
heusserm <matt.heusser@gmail.com>
2018-11-14 18:22:47 -0500

    revised resizing code

--- Webdriver/screenshots/screenshot_maximize_chrome.rb
+++ Webdriver/screenshots/screenshot_maximize_chrome.rb
@@ -1,10 +1,10 @@
 require "selenium-webdriver"
 
 options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--disable-popup-blocking')
-options.add_argument('--disable-translate')
-
+#options.add_argument('--ignore-certificate-errors')
+#options.add_argument('--disable-popup-blocking')
+#options.add_argument('--disable-translate')
+options.add_argument('--start-maximized')
 
 driver = Selenium::WebDriver.for :chrome, options: options
 
@@ -13,10 +13,13 @@ directory = File.expand_path File.dirname(__FILE__)
 specific_filename = "file://" + directory + "/sample.html"
 driver.navigate.to specific_filename
 
+
+target_size = Selenium::WebDriver::Dimension.new(1440, 1800)
+driver.manage.window.size = target_size
+
 puts("Press ENTER to continue ...");
 waitforit = gets();
 
-driver.manage.window.maximize
 driver.save_screenshot("screenshot_orig.png");
 
 
